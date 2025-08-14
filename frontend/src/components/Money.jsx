4d5ffbf78345b9be1e3be06151302848ee322cd5
anankani164 <a.nankani@nankanistudios.com>
2025-08-14 19:37:39 +0000

    Add files via upload

---
+++ frontend/src/components/Money.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { fmtMoney } from '../utils/format';
+
+/**
+ * <Money value={12345.67} /> -> "GHS 12,345.67"
+ * Optional: <Money value={n} currency="GHS" decimals={2} useSymbol/>
+ */
+export default function Money({ value, currency='GHS', decimals=2, useSymbol=false, style={}, className='' }){
+  try{
+    return <span className={className} style={style}>{fmtMoney(value, currency, {decimals, useSymbol})}</span>;
+  }catch(_){
+    return <span className={className} style={style}>{value}</span>;
+  }
+}
